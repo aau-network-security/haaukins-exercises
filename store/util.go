2141c69c69d2ae8802da13d91b16df3c83567dc3
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2020-11-22 11:25:24 +0100

    created add category and exercise functions

---
+++ store/util.go
@@ -0,0 +1,9 @@
+package store
+
+import "github.com/aau-network-security/haaukins-exercises/model"
+
+func checkExerciseFields(ex model.Exercise) error {
+
+	//todo check exercise has at the least the required fields
+	return nil
+}
